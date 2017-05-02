539eb3f50d72e8a143ad152999e713cc97af1520
phantomphysicist <andersge92@gmail.com>
2017-05-02 17:49:34 +0200

    further work with program

--- prosjektoppg/opg_4.py
+++ prosjektoppg/opg_4.py
@@ -1,35 +1,47 @@
 """This program aims to simulate oscillations in a string"""
 
-from math import pi, sin
 import matplotlib.pyplot as plt
+import matplotlib.animation as man
 import numpy as np
 
-#initializing variables in arrays
+##defining functions
+
+def Extremal(i, t):
+    if i == 0:
+        return (dt**2/mi[i])*(-ki[i]*y[i, t] + ki[i]*y[i+1, t]) + 2*y[i, t] - y[i, t-1]
+    elif i == N-1:
+        return (dt**2/mi[i])*(-ki[i-1]*y[i, t] + ki[i-1]*y[i-1, t]) + 2*y[i, t] - y[i, t-1]
+    else:
+        print "input wrong position"
+        quit()
+
+def NextY(i, t):
+    return (dt**2/mi[i])*( -(ki[i-1]+ki[i])*y[i, t] + ki[i-1]*y[i-1, t] + ki[i]*y[i+1, t]) + 2*y[i, t] - y[i, t-1]
+
+##initializing variables
 N = 200 # number of timesteps
 m = 0.02 #kg mass of single points
 k = 10 #kg s^-2 spring constant
 dt = 0.8 * np.sqrt(float(m/k)) # setting timestep. To ensure proper resolution, scaled with angular frequency.
 nt = 1200 # number of timesteps
+##setting variables in arrays
 mi = np.zeros(N) + m # mass array for mass points
 ki = np.zeros(N - 1) + k # spring constant array for springs
-y0 = np.zeros((N, nt)) # initial position state. Later to be current. box-arry to store time states
+y = np.zeros((N, nt)) # initial position state. Later to be current. box-arry to store time states
 i = np.linspace(0, N-1, N) # index array. usable as position along x-axis
-y0[:,0] = np.sin(7 * pi * (i / (N - 1)))
-print i
-# setting initial states. static start, ensures previous state equal.
-y_l = y0[:,0] # "y-last"
+y[:,0] = np.sin(7 * np.pi * (i / (N - 1)))
 
+### manual 1st step
+y[0, 1] = Extremal(0, 1)
+y[-1, 1] = Extremal(N-1, 1)
+for j in range(1, N-1):
+    y[j, 1] = NextY(j, 1)
+    
+### finishing from 2nd timestep and on
+for t in range(1, nt-1):
+    y[0, t] = Extremal(0, t)
+    y[-1, t] = Extremal(N - 1, t)
+    for j in range(1, N - 1):
+        y[j, t] = NextY(j, t)
 
-# initializing "current" array
-y_n = np.zeros(N) # "y-next" next position state
-
-#nested loop updating all points in y per time
-# for t in range(1):
-#     y_n0
-#     y_nN
-#     for i in range(1, N-1):
-#         y_n[i]
-#     y_l = y_0
-#     y_0 = y_n
-#
-# plt.plot(i, y_0)
+### plotting animation of waves
